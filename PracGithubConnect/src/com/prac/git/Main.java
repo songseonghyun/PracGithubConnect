bdf793fe847b88706b8daf38286e2ae6c7e096d4
user <user@130.1.3.65>
2022-06-17 08:16:07 +0900

    Test for GitHub commit and push with Programmers Quiz

--- PracGithubConnect/src/com/prac/git/Main.java
+++ PracGithubConnect/src/com/prac/git/Main.java
@@ -1,5 +1,6 @@
 package com.prac.git;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 //Programmers
@@ -8,13 +9,14 @@ public class Main {
 		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
 		int[] moves = {1,5,3,5,1,2,1,4};
 		
-		System.out.println(solution(board, moves));
+//		System.out.println(solution(board, moves));
+		
+		
 	}
 	
 	//크레인 인형 뽑기
 	private static int solution(int[][] board, int[] moves) {
 		int answer = 0;
-		
 		Stack<Integer> basket = new Stack<Integer>();
 		
 		for (int item : moves) {
@@ -44,3 +46,4 @@ public class Main {
 		return answer;
 	}
 }
+//sure
